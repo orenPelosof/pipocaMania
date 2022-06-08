88f4309f6d433a216a67a225205b286611b9e34b
Anac5823 <ana.c5823@gmail.com>
2022-06-08 16:36:51 -0300

    atualizacao da tela home, inserindo acao no botao na tela de cadastro

---
+++ PipocaMania/PipocaMania/Features/Home/HomeCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeCollectionViewCell.swift
+//  PipocaMania
+//
+//  Created by Ana Luiza on 6/8/22.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+ // receber os componentes
+   
+    @IBOutlet var pontuacaoFilme: UIView!
+    @IBOutlet var tituloFilme: UIView!
+    @IBOutlet var textoFilme: UILabel!
+    @IBOutlet var estrelaFilme: UIImageView!
+    @IBOutlet var notaFilme: UILabel!
+    @IBOutlet var posterFilme: UIImageView!
+    
+}
+
+
+
+
---
+++ PipocaMania/PipocaMania/Features/Home/HomeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeViewController.swift
+//  PipocaMania
+//
+//  Created by Ana Luiza on 6/8/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    // criacao da collection view
+    @IBOutlet var CollectionView: UICollectionView!
+    
+    var ArrayFilmes = ["Star Wars: Os Últimos Jedi", "Vingadores", "Liga da Justiça"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        CollectionView.dataSource = self
+        CollectionView.delegate = self
+        
+    }
+    
+}
+
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return ArrayFilmes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = CollectionView.dequeueReusableCell(withReuseIdentifier: "filmeCell", for: indexPath) as! HomeCollectionViewCell
+        cell.posterFilme.image = UIImage(named: ArrayFilmes[indexPath.item])
+        cell.textoFilme.text = ArrayFilmes[indexPath.item]
+        return cell
+        
+    }
+}
