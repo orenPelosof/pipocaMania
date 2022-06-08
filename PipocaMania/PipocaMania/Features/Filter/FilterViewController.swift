dfb26d19ed6973cc6d7e9be19d51855e07cb9341
Oren Pelosof <orenpelosof@gmail.com>
2022-06-08 10:47:19 -0300

    Segmented filmes e series funcionando

--- PipocaMania/PipocaMania/Features/Filter/FilterViewController.swift
+++ PipocaMania/PipocaMania/Features/Filter/FilterViewController.swift
@@ -8,26 +8,37 @@
 import UIKit
 
 class FilterViewController: UIViewController {
-
+    
     @IBOutlet var filterSearchBar: UISearchBar!
     @IBOutlet var filmesSeriesSegmentedControl: UISegmentedControl!
-    @IBOutlet var moviesCollectionView: UICollectionView!
+    @IBOutlet var postersCollectionView: UICollectionView!
     
     var seriesImages: [String] = ["strangerthings", "arrow", "elite", "flash", "moonknight"]
     
-    var moviesImages: [String] = ["sonic", "batman", "doctorstrange","morbius"]
+    var filmesImages: [String] = ["sonic", "batman", "doctorstrange","morbius"]
+    
+    var imagensIniciais: [String] = []
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        moviesCollectionView.delegate = self
-        moviesCollectionView.dataSource = self
+        //        title = "oi"
+        
+        postersCollectionView.delegate = self
+        postersCollectionView.dataSource = self
         
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func segmentSelectedAction(_ sender: Any) {
+        let segmentSelected = filmesSeriesSegmentedControl.selectedSegmentIndex
+        if segmentSelected == 0 {
+            title = "Buscar Filmes"
+        } else {
+            title = "Buscar Séries"
+        }
+    }
+    
 }
 
 extension FilterViewController: UICollectionViewDelegate {
@@ -37,15 +48,26 @@ extension FilterViewController: UICollectionViewDelegate {
 extension FilterViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return seriesImages.count
+        let segmentSelected = filmesSeriesSegmentedControl.selectedSegmentIndex
+        if segmentSelected == 0 {
+            return filmesImages.count
+        } else {
+            return seriesImages.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "posterCell", for: indexPath) as? FilterCollectionViewCell else { return UICollectionViewCell()}
-        cell.configureCell(seriesImages.sorted()[indexPath.row])
-        return cell
-        
+        let segmentSelected = filmesSeriesSegmentedControl.selectedSegmentIndex
+        if segmentSelected == 0 {
+            cell.configureCell(filmesImages.sorted()[indexPath.row])
+            postersCollectionView.reloadData()
+            return cell
+        } else {
+            cell.configureCell(seriesImages.sorted()[indexPath.row])
+            postersCollectionView.reloadData()
+            return cell
+        }
     }
     
     
